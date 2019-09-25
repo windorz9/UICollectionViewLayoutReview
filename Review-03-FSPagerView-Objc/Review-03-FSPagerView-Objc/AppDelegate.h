fdca98615c75e431acf84178d03875a51b98952c
windorz9 <1925449823@qq.com>
2019-09-25 10:27:47 +0800

    添加 FSPagerControl

---
+++ Review-03-FSPagerView-Objc/Review-03-FSPagerView-Objc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Review-03-FSPagerView-Objc
+//
+//  Created by windorz on 2019/9/24.
+//  Copyright © 2019 Q. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
